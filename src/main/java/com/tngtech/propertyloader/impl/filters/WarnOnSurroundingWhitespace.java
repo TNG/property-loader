89bb5dad58b2730c1d1f9c83821f0f9ee8cf2101
matthias <matthias.bollwein@tngtech.com>
2013-10-01 16:42:30 +0200

    propertyloader filter tests

--- src/main/java/com/tngtech/propertyloader/impl/filters/WarnOnSurroundingWhitespace.java
+++ src/main/java/com/tngtech/propertyloader/impl/filters/WarnOnSurroundingWhitespace.java
@@ -12,7 +12,7 @@ public class WarnOnSurroundingWhitespace extends ValueModifyingFilter {
     protected String filterValue(String key, String value, Properties properties) {
         // don't show lines that end with "\n", that is ok and sometimes needed
         if (!value.trim().equals(value) && !value.endsWith("\n")) {
-            log.warn("Key " + key + "is mapped to '" + value + "', " +
+            log.warn("Key " + key + " mapped to '" + value + "', " +
                     "containing whitespace at the end. You probably do not want this.");
         }
 
--- src/test/java/com/tngtech/propertyloader/impl/filters/ThrowIfPropertyHasToBeDefinedTest.java
+++ src/test/java/com/tngtech/propertyloader/impl/filters/ThrowIfPropertyHasToBeDefinedTest.java
@@ -3,14 +3,34 @@ package com.tngtech.propertyloader.impl.filters;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Properties;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class ThrowIfPropertyHasToBeDefinedTest {
+
+    private ThrowIfPropertyHasToBeDefined throwIfPropertyHasToBeDefined;
+    private Properties properties;
+
     @Before
     public void setUp() throws Exception {
-
+        throwIfPropertyHasToBeDefined = new ThrowIfPropertyHasToBeDefined();
+        properties = new Properties();
+        properties.put("key1","<HAS_TO_BE_DEFINED>");
+        properties.put("key2","<HAS_TO_BE_DEFINED>");
     }
 
     @Test
     public void testFilterValue() throws Exception {
+        try{
+            throwIfPropertyHasToBeDefined.filter(properties);
+            fail("should throw exception");
+        }
+        catch(ThrowIfPropertyHasToBeDefinedException e) {
+            assertTrue(e.toString().contains("Configuration incomplete: property key1 is still mapped to <HAS_TO_BE_DEFINED>"));
+            assertTrue(e.toString().contains("Configuration incomplete: property key2 is still mapped to <HAS_TO_BE_DEFINED>"));
+        }
 
     }
 }
